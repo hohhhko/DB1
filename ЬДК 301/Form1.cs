7f44620ff2d06fdbfc669264e094ff340c88472f
Daniil <Dedkek546@Gmail.com>
2023-09-13 09:30:00 +0400

    с 2 фото

---
+++ ЬДК 301/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace ЬДК_301
+{
+    public partial class Form1 : Form
+    {
+        private SQLiteConnection DB;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            DB = new SQLiteConnection(DataBase.ConnetctionString);
+            await DB.OpenAsync();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter("SELECT * FROM Peapol", DB);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+
+            dataAdapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            dataAdapter.Fill(ds, "Info");
+            dataGridView1.DataSource = ds.Tables[0];
+
+        }
+    }
+    
+}
+
---
+++ ЬДК 301/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ЬДК_301
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+  
+    }
+    static class DataBase
+    {
+        public static string ConnetctionString = @"Data Source= DBPeploe.db; Integrated Security=False; MultipleActiveResultSets=True";
+    }
+    #region table
+
+    static class Table_Peapol
+    {
+        public static string main = "Peapol";
+        public static string ID = "ID";
+        public static string Name = "Name";
+        public static string Photo = "Photo";
+        public static string Surname = "Surname";
+
+    }
+    static class Table_sqlite_sequence
+    {
+        public static string main = "sqlite_sequence";
+        public static string name = "name";
+        public static string seq = "seq";
+    }
+    #endregion
+
+}
+
