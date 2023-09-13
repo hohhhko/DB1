bbf461896ccf075e8a8e97880c92e6621ce1d57b
Daniil <Dedkek546@Gmail.com>
2023-09-13 11:22:17 +0400

    10 фото

--- ЬДК 301/Form1.Designer.cs
+++ ЬДК 301/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(136, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(765, 510);
+            this.dataGridView1.Size = new System.Drawing.Size(769, 511);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
@@ -56,11 +56,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(917, 549);
+            this.ClientSize = new System.Drawing.Size(917, 535);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Вывод данных";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
